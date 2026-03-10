7265098d7aca98b9377f341ce61db6510ee05131
Jeremiah Agware <miahspremium@gmail.com>
2026-03-10 23:32:17 +0100

    work

--- build_ga.sh
+++ build_ga.sh
@@ -24,14 +24,6 @@ while [[ $# -gt 0 ]]; do
         PROJECT_ID="$2"
         shift 2
         ;;
-    --repo-region)
-        REPO_REGION="$2"
-        shift 2
-        ;;
-    --repo-name)
-        REPO_NAME="$2"
-        shift 2
-        ;;
     *)
         echo "Unknown argument: $1"
         exit 1
@@ -44,13 +36,12 @@ if [[ -n "${REGION:-}" && -n "${ZONE:-}" ]]; then
     exit 1
 fi
 
-if [[ -z "${REGION:-}" && -z "${ZONE:-}" ]] || [[ -z "${CLUSTER:-}" ]] ||
-    [[ -z "${PROJECT_ID:-}" ]] || [[ -z "${REPO_REGION:-}" ]] || [[ -z "${REPO_NAME:-}" ]]; then
-    echo "Error: --cluster, --project-id, --repo-region, --repo-name and either --region or --zone are required"
+if [[ -z "${REGION:-}" && -z "${ZONE:-}" ]] || [[ -z "${CLUSTER:-}" ]] || [[ -z "${PROJECT_ID:-}" ]]; then
+    echo "Error: --cluster, --project-id and either --region or --zone are required"
     exit 1
 fi
 
-REGISTRY="${REPO_REGION}-docker.pkg.dev/${PROJECT_ID}/${REPO_NAME}"
+REGISTRY="us-central1-docker.pkg.dev/inhouz-dev/inhouzio-dev-repo"
 IMAGE="${REGISTRY}/redis_test_ms"
 
 # Build & Push
@@ -67,4 +58,4 @@ fi
 
 kubectl apply -f k8s/secret-provider-class.yaml
 kubectl apply -f k8s/deployment.yaml
-kubectl set image deployment/redis-test-ms-deployment redis-test-ms="${IMAGE}:${SHA}" -n devubectl set image deployment/redis-test-ms-deployment redis-test-ms="${IMAGE}:${SHA}" -n dev
+kubectl set image deployment/redis-test-ms-deployment redis-test-ms="${IMAGE}:${SHA}" -n dev
